c9f3bd8823898c06b1eecf51566be4c992d97e32
Mst Rinky Akter <101060713+mstrinkyakter@users.noreply.github.com>
2023-10-05 16:42:04 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$db_name="mysql:host=localhost;dbname=crud";
+$username="root";
+$password="";
+
+$conn=new PDO($db_name,$username,$password);
+// if($conn){
+//     echo "connect";
+// }
+// else{
+//     echo "not connect";
+// }
+?>
