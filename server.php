cc272623836048ef1ba59afbb83d7e3cce2c5868
Mst Rinky Akter <101060713+mstrinkyakter@users.noreply.github.com>
2023-10-05 16:42:45 +0600

    Add files via upload

--- server.php
+++ server.php
@@ -1,29 +1,12 @@
-
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $marks = $_POST['marks'];
-        //    echo $marks;
-        // Define the grading system using if-else conditions
-        if ($marks <= 39) {
-            $grade ='D';
-        } elseif ($marks <= 49) {
-            $grade = 'C';
-        } elseif ($marks <=59 ) {
-            $grade ='B';
-        } elseif ($marks <=69 ) {
-            $grade ='A-' ;
-        } elseif ($marks <= 79) {
-            $grade = 'A';
-        } elseif ($marks>80) {
-            $grade ='A+' ;
-        } else {
-            $grade = 'F';
-        }
 
-        // Display the grade
-        echo "Your Grade is: $grade";
-    }
-    else{
-        echo'invalid method';
-    }
-    ?>
+include "config.php";
+
+    global $conn;
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    echo "<h1> $name,$email,$password </h1>";
+    $conn->exec("INSERT INTO crud_operation (name,email,password) VALUES ('$name','$email','$password')");
+    
+?>
